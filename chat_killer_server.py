7e3a75dc6abc96d320583949c813955c245a1ea5
ClaveilleH <hugo.claveille@etu.univ-cotedazur.fr>
2024-05-09 18:43:20 +0200

    aa

--- chat_killer_server.py
+++ chat_killer_server.py
@@ -1,200 +1,315 @@
-#TODO(done) : ALARM MSG !!BEAT au serv ;
-#TODO(done) : soit !!cookie soit !!pseudo pour gerer les crashs
-#TODO(done) : Recevoir cookie (avec select) 
-#TODO(done) : Encapsulation des saisies client (!!message messageduclient)
-#TODO(done) : Tolérance aux pannes des terminaux qui exécutent les commandes tail -f LOG et cat > TUBE, relance des terminaux et des commandes : 16/20
-#TODO(done) :Tolérances aux pannes de serveur (détection par échec d'envoi de message au serveur), commande !reconnect : 19/20
+import sys, os, signal, socket, select, random, time
+"""
+Script du jeu coté serveur
+https://webusers.i3s.unice.fr/~elozes/enseignement/systeme/
+127.0.0.1
+python3 chat_killer_server.py 127.0.0.1 25565
+"""
+HOST = "127.0.0.1" 
+MAXBYTES = 4096
+BEAT_TIMEOUT = 5
+BEAT_CHECK = 3
 
 
-import os, socket, sys, select, time,signal
+def console(server):
+    line = os.read(0, MAXBYTES).decode()
+    if line[0] == '!':
+        line = line[1:]
+        if line == "quit\n":
+            print("Closing all connections and server...")
+            for c in server.socketList:
+                if c != server.socket and c != 0:
+                    c.close()
+
+                    server.nb_clients -= 1
+            server.socket.close()
+
+        elif line == "list\n":
+            print(server.get_list())
+        
+    elif line[0] == '@':
+        pseudo, command = line.split(1)[0]
+        if pseudo in server.dicoPseudo.keys():
+            client = server.dicoPseudo[pseudo]
+            if client.socket in server.socketList:
+                pass
+            else:
+                print(f"Le client {pseudo} n'est pas connecté.")
+                return
+            
+            if command == '!ban':
+                client.send(str("!!BAN\n").encode())
+                server.disconnect_client(client.sock)
+            elif command == '!suspend':
+                client.send(str("!!MUTE\n").encode())
+            elif command == '!forgive':
+                client.send(str("!!UNMUTE\n").encode())
+            else:
+                print("Commande invalide")
 
 
-def alarm_hdler(sig_num,frame): #Handler qui gère le heartbeat. Il envoie un "!!BEAT" au server toutes les secondes
-	global server
-	global server_statut
-	msg="!!BEAT"
-	try :
-		server.send(msg.encode('utf-8')) #detection d'erreur
-		server_statut= True
-		signal.alarm(1)
-	except :
-		server_statut = False
-	
+        else:
+            print(f"Le pseudo {pseudo} n'existe pas.")
+        
+            
+    else:
+        if line.split()[0] == 'wall':
+            msg = str("server: " + line.split(' ', 1)[1]).encode()
+            server.mess_all(msg)
+        else:
+            print(line)
 
 
-	
-def server_connection(): #Protocole de gestion des connexions client-server
-	HOST = "127.0.0.1"
-	PORT = 2000
-	sockaddr = (HOST, PORT)
-	global server
-	global server_statut
-	global COOKIE
-	if (os.path.exists(pathcookie)):
-		COOKIE = True
-	try:
-		server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4, TCP
-		server.connect(sockaddr)
-		if COOKIE :							#Protocole de reconnection si présence de cookie
-			fd = os.open(pathcookie, os.O_RDONLY)
-			fdr=os.read(fd,MAXBYTES).decode()
-			print("Envoie du cookie au server... ")
-			server.send(str('!!cookie '+fdr).encode())
-			os.close(fd)
-		else :								#Protocole de premiere connection
-			server.send(str('!!pseudo ' + pseudo).encode())
-			fd=os.open("/tmp/"+pseudo+".cookie", os.O_WRONLY|os.O_CREAT)
-			print("En attente de reception de cookie... ")
-			cookie=server.recv(MAXBYTES).decode('utf-8')
-			print(cookie)
-			cookie = cookie.split()[1]
-			os.write(fd,cookie.encode())
-			os.close(fd)
-		signal.signal(signal.SIGALRM, alarm_hdler) #Setup le HEARTBEAT
-		signal.alarm(1)
-		print('connected to:', sockaddr)
-		socketlist = [server]
-		server_statut = True
-		return True,socketlist,server
-	except socket.error as e:
-		print('erreur connexion:', e)
-		sys.exit(1)
-
-
-
-def help(fd): #Affichage des commandes en ligne
-    os.write(fd,"liste des commandes: \n".encode('utf-8'))
-    os.write(fd,"	!quit pour quitter \n".encode('utf-8'))
-    os.write(fd,"	!list pour lister les utilisateurs \n".encode('utf-8'))
-    os.write(fd,"	!help pour afficher la liste des commandes \n".encode('utf-8'))
-    os.write(fd,"	@pseudo message pour envoyer un message privé à :pseudo: \n".encode('utf-8'))
-    os.write(fd,"	message pour envoyer un message public \n".encode('utf-8'))
+def message_client(sock, server):
+    """
+    Fonction qui traite les messages des clients
+    """
+    try:
+        message = sock.recv(MAXBYTES)
+    except OSError as e:
+        print("Erreur: ", e)
+        server.disconnect_client(sock)
+        return
     
-def help_offline(fd):#Affichage des commandes hors ligne
-	os.write(fd,"Vous n'êtes plus connecté au server \n\n".encode('utf-8'))
-	os.write(fd,"Voici la liste des commandes hors ligne: \n".encode('utf-8'))
-	os.write(fd,"	!quit pour fermer le client \n".encode('utf-8'))
-	os.write(fd,"	!help pour afficher la liste des commandes \n".encode('utf-8'))
-	os.write(fd,"	!reconnect pour essayer de vous reconnecter au server\n".encode('utf-8'))
+    client = server.dicoClients[sock]
 
-def term_saisie():    #tant que run == True, fermer un term le relance tout de suite
-	global pathfifo
-	while True :
-		pid = os.fork()
-		if pid == 0:
-			argv=["xterm","-e","cat > "+pathfifo]
-			os.execvp("xterm",argv) #lance le terminal ou entree standard > fifo
-		else :
-			os.wait()
-			
+    if len(message) == 0:
+        pass
+    else:
+        text = message.decode()
+        if text[:2] == "!!":
+            text = text[2:]
+            if text == "BEAT":
+                client.last_beat = time.time()
+                client.send(str("!!BEAT\n").encode())
 
-def term_affichage():	#tant que run == True, fermer un term le relance tout de suite
-	global pathlog
-	while True :
-		pid = os.fork()
-		if pid == 0:
-			argv1 =["xterm","-e","tail -f "+pathlog]
-			os.execvp("xterm",argv1)
-		else :
-			os.wait()
+            elif text == "QUIT\n" or text == "quit\n":
+                server.disconnect_client(sock)
+                print(f"Client {client.pseudo} disconnected")
+                server.mess_all(f"[-]{client.pseudo}!\n".encode())
+
+            elif text[:8] == "message ":
+                text = text[8:]
+
+                if text[0] == '@': # whisper
+                    if not ' ' in text:
+                        print("Erreur: message invalide")
+                        return
+                    
+                    pseudo, message = text.split(' ', 1)
+                    pseudo = pseudo[1:]
+
+                    if pseudo == 'admin':
+                        print("Message de " + client.pseudo + " : " + message)
+
+                    elif pseudo in server.dicoPseudo.keys():
+                        server.dicoPseudo[pseudo].send(str(f"(wisper){server.dicoClients[sock][2]}: {message}").encode())
+
+                    else:
+                        client.send(str("Le pseudo n'existe pas\n").encode())
+                else:
+                    msg = str(f"{client.pseudo}: {text}").encode()
+                    server.mess_all(msg)
+            else:   
+                print("->>>>" + text)
+        elif text[0] == '!':
+            text = text[1:]
+            if text == "list\n":
+                msg = server.get_list() + '\n'
+                client.send(msg.encode())
+            else:
+                print("-2>>>>" + text)
+        else:
+            print("---->" + text)
+
+class Client:
+    def __init__(self, address, socket, pseudo, cookie, last_beat) -> None:
+        self.address = address
+        self.socket = socket
+        self.pseudo = pseudo
+        self.cookie = cookie
+        self.last_beat = last_beat
+    
+    def send(self, msg):
+        self.socket.send(msg)
 
 
-def lancement_client(run,socketlist,server): #Protocol de communication client-server
-	os.mkfifo(pathfifo) #tube nommé pour communication entre terminal de saisie et superviseur
-	pid = os.fork()
-	quit = False
-	if pid == 0:
-		term_saisie()
-	else :
-		fifo=os.open(pathfifo,os.O_RDONLY) #descripteur de fichier fifo readonly
-		socketlist.append(fifo)
-		log=os.open(pathlog, os.O_APPEND|os.O_TRUNC|os.O_CREAT|os.O_WRONLY) #descripteur de fichier log append, create if not exist, supprime le contenu à l'ouverture
-		pid2= os.fork()
-		if pid2== 0:
-			term_affichage()
-		else:
-			global server_statut
-			help(log)
-			while run :
-				while server_statut :
-					(activesockets, _, _) = select.select(socketlist, [], [])
-					for s in activesockets:
-						
-						if s == fifo:
-							line = os.read(fifo, MAXBYTES)
-							lineD = line.decode()
-							if len(line)==0:
-								continue
-							elif lineD[0] == '!':
-								if lineD == "!quit\n":
-									server.close()
-									run = False
-									server_statut = False
-									quit = True
-									os.kill(pid,signal.SIGQUIT)
-									os.kill(pid2,signal.SIGQUIT)
-									break
-								elif lineD == "!list\n":
-									server.send(line)
-								elif lineD == "!help\n":
-									help(log)
-								else:
-									os.write(log,"commande inconnue\n".encode('utf-8'))
-							else:
-								line=("!!message "+line.decode()).encode()
-								server.send(line)
-						else:
-							data = server.recv(MAXBYTES)
-							if len(data) == 0:
-								# run = False
-								continue
-							elif data.decode() != "!!BEAT\n":
-								os.write(log, data)
-								
-				help_offline(log)
-				offline = True
-				
-				while offline and not(quit):
-					line = os.read(fifo, MAXBYTES)
-					lineD = line.decode()
-					if len(line)==0:
-						continue
-					elif lineD[0] == '!':
-						if lineD == "!quit\n":
-							server.close()
-							run = False
-							server_statut = False
-							quit = True
-							os.kill(pid,signal.SIGQUIT)
-							os.kill(pid2,signal.SIGQUIT)
-							break
-						elif lineD == "!help\n":
-							help_offline(log)
-						elif lineD == "!reconnect\n":
-							run,socketlist,server=server_connection()
-							offline = False
-							socketlist.append(fifo)
-							
-						else:
-							os.write(log,"commande inconnue\n".encode('utf-8'))
-			os.close(fifo)
-			os.close(log)
-			os.system("rm "+pathfifo)
-			os.system("rm "+pathlog)
-			os.system("rm "+pathcookie)
-			os.system("pkill xterm") #ferme tous les processus xterm
-			sys.exit(0)
+class Server:
+    def __init__(self, serversocket) -> None:
+        
+		
+
+
+        self.nb_clients = 0
+        self.socket = serversocket
+        self.socketList = [serversocket, 0] # liste des sockets à surveiller
+        self.dicoPseudo = {} # dictionnaire Pseudo -> socket
+        self.dicoClients = {} # dictionnaire socket -> Client
+
+    def get_client(self, id):
+        """
+        Retourne le client associé au pseudo
+        """
+        if isinstance(id, socket.socket):
+            return self.dicoClients[id]
+        elif isinstance(id, str):
+            return self.dicoPseudo[id]
+        
+
+
+    def get_list(self):
+        txt = "Liste des clients :"
+        for pseudo in self.dicoPseudo.keys():
+            txt += '\n' + pseudo + '\t| '
+            if self.dicoPseudo[pseudo].socket in self.socketList:
+                txt += "CONNECTED"
+            else:
+                txt += "DISCONNECTED"
+        return txt
+
+    def mess_all(self, msg):
+        """
+        Envoi le message msg à tous les clients
+        :msg: message à envoyer (bytes)
+        :server.socketList: liste des sockets à qui envoyer le message
+        :sendersocket: socket qui a envoyé le message
+        """
+        for sock in self.socketList:
+            if sock != self.socket and sock != 0:
+                try:
+                    sock.send(msg)
+                except BrokenPipeError:
+                    print("Erreur: client introuvable")
+                    self.disconnect_client(sock)
+                except Exception as e:
+                    print("Erreur: ", e)
+    
+    def disconnect_client(self, sock):  
+        """
+        deconnecte un client
+        ne supp pas des dico car le client peut se reconnecter
+        :c: socket du client à déconnecter
+        :server.socketList: liste des sockets à surveiller
+        """
+        sock.close()
+        self.socketList.remove(sock)
+        self.nb_clients -= 1
+    
+    def new_client(self):
+        (clientsocket, address) = self.socket.accept()
+        
+        self.socketList.append(clientsocket)
+        #! faire un select ppur eviter de bloquer
+        txt = clientsocket.recv(MAXBYTES).decode()
+        print(txt)
+        
+        if txt[:8] == "!!cookie":
+            cookie = txt.split()[1]
+            
+            cookie = cookie # on enleve le \n
+            for client in self.dicoClients.values():
+                if client.cookie == cookie: # on retrouve le client
+                    self.socketList.remove(client.socket)
+                    self.socketList.append(clientsocket)
+                    client.last_beat = time.time()
+                    client.socket = clientsocket # on met à jour le socket
+                    self.dicoClients[clientsocket] = client
+                    
+                    return
+            print("Erreur: cookie invalide")
+                
+
+        elif txt[:8] == "!!pseudo":
+            pseudo = txt.split()[1]
+            if pseudo in self.dicoPseudo.keys():
+                clientsocket.send(str("!!wrong_pseudo\n").encode())
+                return
+
+            cookie = str(random.randint(1000000, 9999999))
+            
+            client = Client(address, clientsocket, pseudo, cookie, time.time())
+
+            self.dicoPseudo[pseudo] = client
+            self.dicoClients[clientsocket] = client
+
+            self.nb_clients += 1
+
+            clientsocket.send(str(f"!!cookie {cookie}\n").encode())
+            print("New client connected")
+            self.mess_all(f"[+]{pseudo}\n".encode())
+        
+        else:
+            self.disconnect_client(clientsocket)
+            print("Erreur: message invalide")
+            return
 
 def main():
-	run,socketlist,servert=server_connection()
-	lancement_client(run,socketlist,server)
+    def alrm_handler(sig, frame):
+        """
+        Verifie si tous les clients ont envoyé un beat
+        """
+        now = time.time()
+        for sock in server.socketList:
+            if sock != server.socket and sock != 0:
+                if sock in server.dicoClients.keys() and now - server.dicoClients[sock].last_beat > BEAT_TIMEOUT:
+                    print("Client {} timeout".format(server.dicoClients[sock].pseudo))
+                    server.disconnect_client(sock)
+        signal.alarm(BEAT_CHECK) # on remet l'alarme
 
-	
+    try:
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4, TCP
+        serversocket.bind((HOST, PORT))
+        serversocket.listen()
+    except OSError as e:
+        print("Error: ", e)
+        sys.exit(1)
+    except Exception as e:
+        print("Error: ", e)
+        sys.exit(1)
+    #! pas sur de ces exceptions
+
+    signal.signal(signal.SIGALRM, alrm_handler)
+    server = Server(serversocket)
+
+    first = True
+    signal.alarm(BEAT_CHECK) # 5 secondes pour envoyer un beat
+    
+    print("Server started")
+    while server.nb_clients > 0 or first:
+        first = False
+        try:
+            (activesockets, _, _) = select.select(server.socketList, [], []) 
+
+        except Exception as e:
+            print("Erreur: ", e)
+            sys.exit(1)
+        #! gerer les exceptions
+
+        for sock in activesockets:
+            if not sock in server.socketList:
+                # le socket a été enlevé entre temps
+                continue
+
+            if sock == serversocket:
+                server.new_client()
+            
+            elif sock == 0:
+                console(server)
+
+            else:
+                message_client(sock, server)
+                
+
+
+
 if __name__ == "__main__":
-	pseudo = input("Entrez votre identifiant: ")
-	pathfifo = "/tmp/"+pseudo+".fifo"
-	pathlog = "/tmp/"+pseudo+".log"
-	pathcookie="/tmp/"+pseudo+".cookie"
-	MAXBYTES = 4096
-	COOKIE = False
-	main()
+    if len(sys.argv) != 2:
+        print("Usage: chat_killer_server.py <port>")
+        sys.exit(1)
+
+    PORT = int(sys.argv[1])
+    
+    
+    main()
+
+    sys.exit(0)
